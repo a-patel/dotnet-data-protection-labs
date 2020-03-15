e22d291767c3c50afdcf80587ff3ce28002780e5
Ashish Patel <toaashishpatel@gmail.com>
2020-03-16 01:03:08 +0530

    Azure Blob Storage - Added blob storage configuration - added appsettings - added config model

---
+++ src/AspNetCoreDataProtectionLabs.AzureBlobStorage/Configuration/AzureBlobStorageConfig.cs
@@ -0,0 +1,13 @@
+﻿namespace AspNetCoreDataProtectionLabs.AzureBlobStorage.Configuration
+{
+    public class AzureBlobStorageConfig
+    {
+        public string StorageAccountBlobBaseUrl { get; set; }
+
+        public string StorageContainerName { get; set; }
+
+        public string StorageBlobName { get; set; }
+
+        public string SharedTokenCacheTenantId { get; set; }
+    }
+}
